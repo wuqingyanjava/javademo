6a9cbd2ded7b3c9b77361a4efe1aa87ad3003744
wqy <wqy@56join.com>
2019-06-05 11:44:33 +0800

    添加swagger优化

--- src/main/java/com/example/servicedemo/controller/DemoController.java
+++ src/main/java/com/example/servicedemo/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.example.servicedemo.controller;
 import com.example.cxfdemo.service.Model;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
